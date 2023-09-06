b1128638185d010622a9194f52756dcca3e7a31a
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-09-06 18:20:25 +0530

    .map() method

---
+++ MRF.js
@@ -0,0 +1,21 @@
+// Map
+// Creates a brand new array by mapping the array to defined function
+
+const twice = 2;
+const arr = [100, 345, 458, 513, 666];
+
+let newArr = arr.map((i) => i * 2);
+console.log(newArr);
+
+// It also has access to the index and the whole array in the form
+// of element, index, array
+let newArr2 = arr.map((ele, i, arr) => {
+  return `Element at ${i + 1}: ${ele}`;
+});
+console.log(newArr2);
+
+// Filter
+// Filters the array via some condition that we define and makes a new array
+
+// Reduce
+// Returns a single value as it reduces the array via some operation defined
