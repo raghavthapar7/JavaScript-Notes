d241f7a97fc881552c2c607c87b8a8cfcef79581
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-09-06 20:08:09 +0530

    .reduce() method

--- MRF.js
+++ MRF.js
@@ -19,11 +19,26 @@ console.log(newArr2);
 // Filter
 // Filters the array via some condition that we define and makes a new array
 
+/* 
 const arr = [100, 345, 458, 513, 666];
 const newArr = arr.filter((ele) => ele > 500);
 console.log(newArr);
 
 const newArr2 = arr.filter((ele) => ele < 400);
 console.log(newArr2);
+ */
+
 // Reduce
 // Returns a single value as it reduces the array via some operation defined
+
+const arr = [100, 345, 458, 513, 666];
+const sum = arr.reduce((acc /* Accumulator */, ele /* Current Element */) => {
+  return acc + ele;
+});
+console.log(sum);
+
+// We can also provide value for the accumulator through this -
+const sumNew = arr.reduce((acc, ele) => {
+  return acc + ele;
+}, 100 /* Initial value of accumulator */);
+console.log(sumNew);
