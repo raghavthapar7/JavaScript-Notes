895e2878328472ad15fb0d53cfb0353fa9dbfa74
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-09-06 19:34:11 +0530

    .filter() method

--- MRF.js
+++ MRF.js
@@ -1,6 +1,7 @@
 // Map
 // Creates a brand new array by mapping the array to defined function
 
+/* 
 const twice = 2;
 const arr = [100, 345, 458, 513, 666];
 
@@ -12,10 +13,17 @@ console.log(newArr);
 let newArr2 = arr.map((ele, i, arr) => {
   return `Element at ${i + 1}: ${ele}`;
 });
-console.log(newArr2);
+console.log(newArr2); 
+*/
 
 // Filter
 // Filters the array via some condition that we define and makes a new array
 
+const arr = [100, 345, 458, 513, 666];
+const newArr = arr.filter((ele) => ele > 500);
+console.log(newArr);
+
+const newArr2 = arr.filter((ele) => ele < 400);
+console.log(newArr2);
 // Reduce
 // Returns a single value as it reduces the array via some operation defined
